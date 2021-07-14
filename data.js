cda18dadb2fc233d5480f60b1b703ef1c9c6afdb
BrayanLopez <balg.lg55@gmail.com>
2021-07-13 21:27:25 -0500

    finished the styles of portfolio

---
+++ data.js
@@ -0,0 +1,45 @@
+let dataProjects = [
+  {
+    title: "Location",
+    description: "An application, developed for the company Location.",
+    date: new Date(),
+    thechnology: ["React", "Boostrap", "HTML", "CSS", "JavaScript"],
+    url: "https://app.qrlocation.com/Login/",
+    image: "./img/qrlocation.png",
+  },
+  {
+    title: "JS Documentation",
+    description: "A documentation of the language JavaScript",
+    date: new Date(),
+    thechnology: ["HTML", "CSS", "JavaScript"],
+    url: "https://brayanlopez.github.io/javascript-simple-documentation",
+    image: "./img/jsdocumentation.png",
+  },
+  {
+    title: "Super Shoes",
+    description:
+      "A tribute page to Dr. Norman Borlaug, based in the page of FCC",
+    date: new Date(),
+    thechnology: ["HTML", "CSS", "JavaScript"],
+    url: "https://brayanlopez.github.io/product-landing-page/",
+    image: "./img/supershoes.png",
+  },
+  {
+    title: "Survey form",
+    description:
+      "A tribute page to Dr. Norman Borlaug, based in the page of FCC",
+    date: new Date(),
+    thechnology: ["HTML", "CSS", "JavaScript"],
+    url: "https://brayanlopez.github.io/survey-form/",
+    image: "./img/surveyform.png",
+  },
+  {
+    title: "Tribute Page",
+    description:
+      "A tribute page to Dr. Norman Borlaug, based in the page of FCC",
+    date: new Date(),
+    thechnology: ["HTML", "CSS", "JavaScript"],
+    url: "https://brayanlopez.github.io/tribute-page/",
+    image: "./img/tributepage.png",
+  },
+];
