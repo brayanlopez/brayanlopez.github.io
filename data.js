2dab987173b505602db8633a87d2a4fae731a7d7
BrayanLopez <balg.lg55@gmail.com>
2021-12-22 22:02:39 -0500

    re-structured project, fixed data and added new images

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
 let dataProjects = [
   {
     title: "Batatabit",
-    description: "A landing page for a company called Batatabit. Based on a platzi course.",
+    description:
+      "A landing page for a company called Batatabit. Based on a Platzi course.",
     date: new Date("22-12-2021"),
     thechnology: ["React", "HTML", "CSS", "JavaScript"],
-    url: "https://brayanlopez.github.io/batatabit",
-    image: "",
+    url: "https://brayanlopez.github.io/Batatabit",
+    image: "batatabit.jpg",
   },
   {
     title: "Location",
@@ -13,15 +14,16 @@ let dataProjects = [
     date: new Date("10-05-2021"),
     thechnology: ["React", "Boostrap", "HTML", "CSS", "JavaScript", "React"],
     url: "https://app.qrlocation.com/Login/",
-    image: "./img/qrlocation.png",
+    image: "qrlocation.png",
   },
   {
     title: "JS Documentation",
-    description: "A basic documentation of the language JavaScript. Based on a FreeCodeCamp project.",
+    description:
+      "A basic documentation of the language JavaScript. Based on a FreeCodeCamp project.",
     date: new Date("02-07-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/javascript-simple-documentation",
-    image: "./img/jsdocumentation.png",
+    image: "jsdocumentation.png",
   },
   {
     title: "Super Shoes",
@@ -30,16 +32,15 @@ let dataProjects = [
     date: new Date("01-07-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/product-landing-page/",
-    image: "./img/supershoes.png",
+    image: "supershoes.png",
   },
   {
     title: "Survey form",
-    description:
-      "A basic survey form. Based on a FreeCodeCamp project.",
+    description: "A basic survey form. Based on a FreeCodeCamp project.",
     date: new Date("24-06-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/survey-form/",
-    image: "./img/surveyform.png",
+    image: "surveyform.png",
   },
   {
     title: "Tribute Page",
@@ -48,6 +49,6 @@ let dataProjects = [
     date: new Date("24-06-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/tribute-page/",
-    image: "./img/tributepage.png",
+    image: "tributepage.png",
   },
 ];
