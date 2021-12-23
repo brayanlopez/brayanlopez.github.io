e1672e1742872a5aea49b1bb50907c71cab660a4
brayanlopez <balg.lg55@gmail.com>
2021-12-22 21:24:43 -0500

    batatabit project added and spelling corrected

--- data.js
+++ data.js
@@ -1,15 +1,23 @@
 let dataProjects = [
   {
+    title: "Batatabit",
+    description: "A landing page for a company called Batatabit. Based on a platzi course.",
+    date: new Date("22-12-2021"),
+    thechnology: ["React", "HTML", "CSS", "JavaScript"],
+    url: "https://brayanlopez.github.io/batatabit",
+    image: "",
+  },
+  {
     title: "Location",
     description: "An application, developed for the company Location.",
     date: new Date("10-05-2021"),
-    thechnology: ["React", "Boostrap", "HTML", "CSS", "JavaScript"],
+    thechnology: ["React", "Boostrap", "HTML", "CSS", "JavaScript", "React"],
     url: "https://app.qrlocation.com/Login/",
     image: "./img/qrlocation.png",
   },
   {
     title: "JS Documentation",
-    description: "A basic documentation of the language JavaScript. Based in a project from FreeCodeCamp",
+    description: "A basic documentation of the language JavaScript. Based on a FreeCodeCamp project.",
     date: new Date("02-07-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/javascript-simple-documentation",
@@ -18,7 +26,7 @@ let dataProjects = [
   {
     title: "Super Shoes",
     description:
-      "A landing page of a shoe store. Based in a project from FreeCodeCamp.",
+      "A landing page of a shoe store. Based on a FreeCodeCamp project.",
     date: new Date("01-07-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/product-landing-page/",
@@ -27,7 +35,7 @@ let dataProjects = [
   {
     title: "Survey form",
     description:
-      "A basic survey form. Based in a project from FreeCodeCamp",
+      "A basic survey form. Based on a FreeCodeCamp project.",
     date: new Date("24-06-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/survey-form/",
