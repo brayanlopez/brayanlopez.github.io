3c9d83348324283f46f03e4242af244483edf6b8
Brayan A. Lopez G. <balg.lg55@gmail.com>
2024-01-07 22:23:41 -0500

    CHANGE: styles for used grid and ADD new data projects

--- src/data.js
+++ src/data.js
@@ -1,10 +1,42 @@
-let dataProjects = [
+const socialNetwork = [
+  { name: "Github", url: "https://github.com/brayanlopez" },
+  { name: "Twitter", url: "https://twitter.com/bryanlopezg5" },
+  { name: "LinkedIn", url: "https://www.linkedin.com/in/brayanlopezg/" },
+];
+
+const dataProjects = [
+  {
+    title: "NFT Preview Card Component",
+    description:
+      "Card component of an NFT. Based on the first Front-end Mentor challenge",
+    date: new Date("01-01-2024"),
+    thechnology: ["HTML", "CSS", "JavaScript"],
+    url: "https://brayanlopez.github.io/NTF-preview-card-component/",
+    image: "default.jpg",
+  },
+  {
+    title: "Markdown Previewer",
+    description:
+      "A markdown previe, change meanwhile the user write markdown code. Based on a FreeCodeCamp project",
+    date: new Date("11-11-2023"),
+    thechnology: ["React", "Material UI", "marked"],
+    url: "https://brayanlopez.github.io/markdown-previewer/",
+    image: "default.jpg",
+  },
+  {
+    title: "Countdown",
+    description: "Countdown app used in a escape mistery game.",
+    date: new Date("19-08-2022"),
+    thechnology: ["React", "Material UI", "marked"],
+    url: "https://brayanlopez.github.io/countdown/",
+    image: "default.jpg",
+  },
   {
     title: "Batatabit",
     description:
       "A landing page for a company called Batatabit. Based on a Platzi course.",
     date: new Date("22-12-2021"),
-    thechnology: ["React", "HTML", "CSS", "JavaScript"],
+    thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/Batatabit",
     image: "batatabit.jpg",
   },
@@ -12,7 +44,7 @@ let dataProjects = [
     title: "Location",
     description: "An application, developed for the company Location.",
     date: new Date("10-05-2021"),
-    thechnology: ["React", "Boostrap", "HTML", "CSS", "JavaScript", "React"],
+    thechnology: ["React", "Boostrap", "HTML", "CSS", "JavaScript"],
     url: "https://app.qrlocation.com/Login/",
     image: "qrlocation.png",
   },
@@ -45,7 +77,7 @@ let dataProjects = [
   {
     title: "Tribute Page",
     description:
-      "A tribute page to Dr. Norman Borlaug. Based in a project from FreeCodeCamp",
+      "A tribute page to Dr. Norman Borlaug. Based on a project from FreeCodeCamp",
     date: new Date("24-06-2021"),
     thechnology: ["HTML", "CSS", "JavaScript"],
     url: "https://brayanlopez.github.io/tribute-page/",
